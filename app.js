487d4bc7b4026106bc31cc02857320999bccc089
jaffar aman <amanjaffar50@gmail.com>
2021-10-28 12:08:33 +0500

    JS array and loop

---
+++ app.js
@@ -0,0 +1,192 @@
+// alert("HELLO CLASS...")
+
+// var userEmail = prompt("Enter your Email");
+// var userPassword = prompt("Enter your Password");
+
+
+
+// if(userEmail !== "" || userPassword !== "" ){
+//     if(userEmail === "jaffar@gmail.com" &&  
+//     userPassword === "12345"){
+//         document.write("Jaffar Successfully Login")
+
+//         }
+// else if(userEmail === "ali@gmail.com" && userPassword === "12345678"){
+// document.write("ALI Successfully Login")
+
+// }
+// else{
+// document.write("email password not match...")
+
+// }
+
+// }
+
+// else{
+//     document.write("your field's is empty")
+    
+// }
+
+
+
+
+
+// var name = "jaffar";
+
+// alert(name);
+
+
+
+// var myName = undefined ;
+
+// console.log(myName)
+
+
+// var myname;
+
+// console.log(myname)
+
+// myname = "jaffar"
+
+// console.log(myname);
+
+
+
+
+    //////Arrays is JS///
+
+// var fruit1 = 'apple';
+// var fruit2 = 'orange';
+// var fruit3 = 'mango';
+// var fruit4 = 'graps';
+
+
+// var  fruits = ["apple" , "orange" , "mango" , "graps"]
+
+
+// console.log(fruits.length)
+
+
+
+// var arr = [] ;
+// console.log(arr)
+
+
+// var  fruits = ["apple" , "orange" , "mango" , "graps"]
+
+
+// var std = []
+
+// // console.log(std)
+
+// std[0]  = "Jaffar Aman";
+// std[1]  = 20;
+// std[2]  = "male";
+
+// console.log(std)
+
+
+
+
+
+// var names = ["Jaffar" , "Ahmed"]
+
+// names[0] = "Jaffar Aman"
+
+// console.log(names[0])
+// console.log(names[1])
+
+
+// console.log( "old array" ,names)
+
+// // names.push("Hanzala")
+// names.pop()
+
+// console.log( "New Array" ,names)
+
+
+    ////POP AND PUSH///
+// var names = ["Jaffar" , "Ahmed"]
+
+// console.log( "old array" ,names)
+
+// names.pop()
+// names.push("Ali")
+    
+// console.log( "New Array" ,names)
+
+
+
+
+
+/////SHIFT AND UNSHIFT///
+// var names = ["Talha" , "Ahmed"]
+
+// console.log( "old array" ,names)
+
+// names.shift()
+// names.unshift("Hanzala")
+
+// console.log( "New Array" ,names)
+    
+
+
+        ////SLICE////
+
+
+// var namesArr = ["Ali" , "Talha" , "Hanzala" , "Ahmed"];
+// console.log( "ARRAY" , namesArr)
+
+// var copyArr = namesArr.slice(2 , 4 )
+
+// console.log( "COPY ARR" , copyArr)
+
+
+
+    ////splice ///
+// var namesArr = ["Ali" , "Talha" , "Hanzala" , "Ahmed"];
+
+// console.log( "ARRAY" , namesArr)
+
+
+// namesArr.splice( 2  , 1 , "Jaffar" , "Aman" ,"Aman","Aman","Aman" )
+
+
+// console.log( "upadte" , namesArr)
+
+
+
+
+
+        /////FOR LOOP////
+
+
+// for( initialization ; condition ; incre/decre  ){
+
+//         ///statements///
+
+// }        
+
+
+// for(var i = 1 ; false ; i++ ){
+//         console.log(i)
+// }
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+
+
+
+for(var i = 1 ; i<=10 ; i++ ){
+
+        document.write("2 x" + " " + i + " " + "=" + " " + 2*i + "<br>" )
+
+}
+
